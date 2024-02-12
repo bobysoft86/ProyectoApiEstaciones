d7d4af357293ef4d7409b4a0adbc768a36ec332a
Robert Rodriguez Carton <robertrodriguezcarton@gmail.com>
2024-02-12 17:32:58 +0100

    cambios estacion

--- src/models/estacion.model.js
+++ src/models/estacion.model.js
@@ -3,8 +3,11 @@ const mongoose = require("mongoose");
 const estacionScheme = new mongoose.Schema({
   
   name: { type: String, unique: true, trim: true, required: true },
-  password: { type: String, trim: true, required: true },
   img: { type: String, trim: true, required: true },
+  capacity:{type: Number,required:true},
+  propertySpace:{ type: String},
+  propertyTipe:{type:String},
+  aviable:{type:Boolean},
   location:{
       latitude:{type:Number},
       longitude:{type:Number}
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,23 +1,20 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const Estacion = require("../models/estacion.model");
 const salt = 10; // complejidad del encriptado
 
-const userSchema = new mongoose.Schema(
-    {
-        name: { type: String, unique: true, trim: true, required: true },
-        password: { type: String, trim: true, required: true },
-        location:{
-            latitude:{type:Number},
-            longitude:{type:Number}
-            },
-    },
-);
+const userSchema = new mongoose.Schema({
+  name: { type: String, unique: true, trim: true, required: true },
+  password: { type: String, trim: true, required: true },
+  img: { type: String, required: true },
+  estaciones: [{ type: mongoose.Schema.Types.ObjectId, ref: "Estacion" }],
+});
 
 userSchema.pre("save", (next) => {
-    if (this.password){
-        this.password = bcrypt.hashSync(this.password, salt)
-    }
-    next();
+  if (this.password) {
+    this.password = bcrypt.hashSync(this.password, salt);
+  }
+  next();
 });
 
 const User = mongoose.model("users", userSchema);
